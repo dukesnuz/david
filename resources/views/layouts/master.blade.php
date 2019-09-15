7e2ce82b27507f6993f849ee46d898763da95cc9
David Petringa <david@ajaxtransport.com>
2019-09-15 19:34:29 -0400

    working on blades

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>
+    @yield('title', 'David.Dukesnuz')
+  </title>
+
+  <meta charset='utf-8'>
+  <meta name="description" content="This page is a list of coding and website turorials which I use and find useful in developing websites.">
+  <meta name="keywords" content="coding, website, developemnt, Laravel, html, css, php, mysql, frameworks, angular, vue.js">
+  <meta name="author" content="David Petringa, Coded September 2019">
+  <meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+  <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
+  <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet'>
+
+  @stack('head')
+
+</head>
+<body>
+  <div class="content">
+    <header>
+      <h1 class="text-primary">David's Famous Coding Links</h1>
+      <p>Under construction</p>
+    </header>
+
+    <section id='main'>
+      @yield('content')
+    </section>
+
+    <footer>
+      <a href='https://github.com/dukesnuz/david'><i class='fa fa-github'></i></a>&nbsp;
+      &copy; {{ date('Y') }}
+    </footer>
+
+    @stack('body')
+
+    <!-- Default Statcounter code for david.dukesnuz.com
+    http://david.dukesnuz.com/ -->
+    <script type="text/javascript">
+    var sc_project=12099896;
+    var sc_invisible=1;
+    var sc_security="2987e810";
+  </script>
+  <script type="text/javascript"
+  src="https://www.statcounter.com/counter/counter.js"
+  async></script>
+  <noscript><div class="statcounter"><a title="Web Analytics"
+    href="https://statcounter.com/" target="_blank"><img
+    class="statcounter"
+    src="https://c.statcounter.com/12099896/0/2987e810/1/"
+    alt="Web Analytics"></a></div></noscript>
+    <!-- End of Statcounter Code -->
+  </div>
+</body>
+</html>
--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -1,19 +1,18 @@
 @extends('layouts.master')
 
-
 @section('title')
-    New book
+  David's Coding Links
 @endsection
 
 @section('content')
 
   <div class="flex-center position-ref full-height">
     <div class="content">
-      <ul class="list-group">
-        @<?php foreach ($urls as $key => $url): ?>
-          <li class="list-group-item">{{ $url->subject }}</li>
-          <li class="list-group-item"><a href="{{ $url->link }}">Read More</a></li>
-          <li class="list-group-item">{{ $url->description }}</li>
+      <ul class="list-group m-5" style="margin-bottom:250px">
+        <?php foreach ($urls as $key => $url): ?>
+          <li class="list-group-item-action">{{ $url->subject }}</li>
+          <li class="list-group-item list-group-item-action list-group-item-success"><a href="{{ $url->link }}">Read More</a></li>
+          <li class="list-group-item m-5 bg-secondary text-warning">{{ $url->description }}</li>
         <?php endforeach; ?>
       </ul>
 
