8cb3ddb424381c10a2cc41dc8612a58958aeba76
David Petringa <david@ajaxtransport.com>
2019-11-20 18:47:39 -0600

    created blade and component to create a blog post, still working on it

---
+++ resources/js/components/blog/CreateBlogPost.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="inner-content">
+    <h3>Create a Blog Post</h3>
+
+    <editor
+      api-key="8fvbyqp6y3crcp6loaupiilair2atyyfei80ao20yezogbuv"
+      cloud-channel="5"
+      :disabled="false"
+      id="uuid"
+      :init="{  }"
+      initial-value
+      model-events
+      plugins
+      tag-name="div"
+      toolbar
+      value
+      v-model="content"
+    ></editor>
+    
+    <p><button @click="create()">Search Here</button></p>
+    
+  </div>
+</template>
+
+<script>
+import Editor from "@tinymce/tinymce-vue";
+export default {
+  name: "app",
+  components: {
+    editor: Editor
+  },
+  data() {
+    return {
+      content: ""
+    };
+  },
+  methods: {
+      create(){
+          console.log(this.content);
+      }
+  }
+};
+</script>
+
+<style scoped>
+.inner-content {
+  padding-bottom: 500px;
+}
+</style>
---
+++ resources/views/blog/create-post.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master-blog')
+
+@section('title')
+Create a Blog Post
+@endsection
+
+@section('content')
+<div class="container">
+  <div id="app">
+        <create-blog-post/>
+  </div>
+</div>
+@endsection
+
+@section('appjs')
+<script src="{{ asset('js/app.js')}}"></script>
+@endsection
