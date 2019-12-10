d6a9ce2c2d226f22c95b2ebf0fe57d3f6fdc1f16
David Petringa <david@ajaxtransport.com>
2019-12-10 13:13:52 -0600

    created a constants file

---
+++ config/constants.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'base_url' => env('BASE_URL', 'david.loc'),
+    'company_name' => 'Dukesnuz',
+    'email_david_petringa' => 'david@davidpetringa.com',
+    'email_dukesnuz' => 'hello@dukesnuz.com',
+
+];
