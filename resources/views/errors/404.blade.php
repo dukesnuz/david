a9948152a32a0cb46fc488cac681bc4467579c3f
David Petringa <david@ajaxtransport.com>
2020-01-08 08:54:04 -0600

    fixed spelling error

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,4 +5,4 @@
 @section('message', __('I apologize for this error. My database was accidentally deleted. All my
 blog posts were lost. No I did not have a back up. Yes this was very foolish of me.  On a positive note,
 I have added a search feature to search all blog posts. I will be re-writing the
-lost and not forgotten blog posts as time permits. As I rebiuld the posts I will post them to social media.'))
+lost and not forgotten blog posts as time permits. As I rebuild the posts I will post them to social media.'))
