e9922cbf294a432de0f3b5d0dedd70c411442918
David Petringa <david@ajaxtransport.com>
2020-01-08 09:50:55 -0600

    edited blog category table seeder

--- database/seeds/BlogcategoriesTableSeeder.php
+++ database/seeds/BlogcategoriesTableSeeder.php
@@ -5,34 +5,40 @@ use Illuminate\Database\Seeder;
 class BlogcategoriesTableSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    * Run the database seeds.
+    *
+    * @return void
+    */
     public function run()
     {
         DB::table('blogcategories')->insert([
-       'created_at' => Carbon\Carbon::now()->toDateTimeString(),
-       'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
-       'categorys' => 'Laravel',
-     ]);
+      'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+      'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+      'categorys' => 'Computer Technology',
+    ]);
 
         DB::table('blogcategories')->insert([
-       'created_at' => Carbon\Carbon::now()->toDateTimeString(),
-       'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
-       'categorys' => 'Transportation',
-     ]);
+      'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+      'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+      'categorys' => 'Transportation',
+    ]);
 
         DB::table('blogcategories')->insert([
-       'created_at' => Carbon\Carbon::now()->toDateTimeString(),
-       'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
-       'categorys' => 'Music',
-     ]);
+      'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+      'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+      'categorys' => 'General',
+    ]);
 
         DB::table('blogcategories')->insert([
-       'created_at' => Carbon\Carbon::now()->toDateTimeString(),
-       'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
-       'categorys' => 'Web Development',
-     ]);
+      'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+      'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+      'categorys' => 'Website Development',
+    ]);
+
+        DB::table('blogcategories')->insert([
+      'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+      'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+      'categorys' => 'History',
+    ]);
     }
 }
