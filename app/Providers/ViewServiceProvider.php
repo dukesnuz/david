897394b26006214b52e65c53536bf0182772d1b2
David Petringa <david@ajaxtransport.com>
2020-01-09 11:01:15 -0600

    updated meta description in view service provider

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -25,9 +25,9 @@ class ViewServiceProvider extends ServiceProvider
     public function boot()
     {
         view::share('title', 'Dukesnuz A Website Development and Technology Blog');
-        view()->share('description', 'Website development and technology blog. Also with a collection of
-        favorite web sites at Dukesnuz. There is a large concentration on web dev.');
-        view()->share('keywords', 'blog, website developmemnt');
+        view()->share('description', 'Website development and web design blog with some
+        computer technology posts. There is a section with a collection of my favorite web sites.');
+        view()->share('keywords', 'blog, website developmemnt, web design');
         view()->share('author', 'David Petringa, Coded December 2019');
     }
 }
