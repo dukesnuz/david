16d64ba4464bac7542abef379b7a6cfcfd3c37dd
David Petringa <david@ajaxtransport.com>
2020-01-23 09:42:05 -0600

    edit kernel commnad to run sitemap generator

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         //below uncomment to run a cron in the kernel
-        $schedule->command('inspire')->everyTenMinutes(); //daily();
+        //$schedule->command('inspire')->everyTenMinutes(); //daily();
+        $schedule->command('sitemap:generate')->everyTenMinutes();
     }
 
     /**
