633b2bb1bbdfd940f109e90d8c4ba2a7cb033ab2
David Petringa <david@ajaxtransport.com>
2020-01-24 15:22:11 -0600

    add email notification when GenerateSitemap cron runs

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -4,6 +4,7 @@ namespace David\Console\Commands;
 
 use Illuminate\Console\Command;
 use Spatie\Sitemap\SitemapGenerator;
+use Mail;
 
 class GenerateSitemap extends Command
 {
@@ -33,5 +34,23 @@ class GenerateSitemap extends Command
         SitemapGenerator::create("http://blog.dukesnuz.com/blog/home")->writeToFile('public/sitemap.xml');
         SitemapGenerator::create("https://david.dukesnuz.com/blog/home")->writeToFile('public/sitemap_david.xml');
         SitemapGenerator::create("http://dukesnuz.com")->writeToFile('public/sitemap_main.xml');
+        $this->emailNotification();
+    }
+
+    public function emailNotification()
+    {
+        $body = "sitemap.xml file was updated used cron and GenerateSitemap pacakge.\r\n";
+        $body .=  date('M-d-Y H:i:s');
+        $data = array(
+      'email' => config('constants.email_dukesnuz'),
+      'emailFrom' => config('constants.email_dukesnuz'),
+      'subject' => "Cron Ran, Sitemap.xml Was updated",
+      'body' => $body,
+    );
+        Mail::raw($data['body'], function ($message) use ($data) {
+            $message->to($data['email']);
+            $message->from($data['emailFrom']);
+            $message->subject($data['subject']);
+        });
     }
 }
